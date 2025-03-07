20e1119c89ecd8376a4d7145abfa304f1428950f
Prince Jain <pj122333444@gmail.com>
2025-03-08 01:50:12 +0530

    server.js

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 import express from "express";
-
-const app = express();
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const port = 3000;
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+  console.log("id", socket.id);
+  socket.broadcast.emit("msg", "yes");
+});
 
 app.get("/", (req, res) => {
   res.send("hellow world wedede");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
